069510501b79221c5a2a7fe52a66126f5630de81
Anand Houston <anand@anandhouston.com>
2022-06-16 15:43:04 +0800

    Added file read/write functions

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,63 @@
+mod state;
 mod to_do;
+
+use std::env;
+use state::{write_to_file, read_file};
+use serde_json::value::Value;
+use serde_json::{Map, json};
 
 use to_do::ItemTypes;
 use to_do::to_do_factory;
 use to_do::structs::traits::create::Create;
 
 fn main() {
-  let to_do_item: Result<ItemTypes, &'static str> =
-    to_do_factory("pending", "wash the dishes");
 
-// Update this match statement with any new status's from Factory
+  // collects the environment arguments passed in by the user and collects to a vector of strings
+  let args: Vec<String> = env::args().collect(); 
+
+  // defines the commands from the environment
+  let status: &String = &args[1];
+  let title: &String = &args[2];
+
+  // read the json file and print it to command line
+  let mut state: Map<String, Value> = read_file("./state.json");
+  println!("State: {:?}", state);
+
+  // insert the title and status into the map
+  state.insert(title.to_string(), json!(status));
+
+  // write the map to the JSON file
+  write_to_file("./state.json", &mut state);
+
+  // creates a pending item called wash the laundry
+  let to_do_item: Result<ItemTypes, &'static str> 
+    = to_do_factory("pending", "wash the laundry");
+
   match to_do_item.unwrap() {
-    ItemTypes::Pending(item) 
-      => item.create(&item.super_struct.title),
-    ItemTypes::Done(item) 
-      => println!("The {} item is completed!", item.super_struct.title),
+    ItemTypes::Pending(item) => item.create(&item.super_struct.title),
+    ItemTypes::Done(item) => println!("it's a done item with the title: {}", item.super_struct.title)
   }
 }
+
+
+
+
+// mod to_do;
+
+// use to_do::ItemTypes;
+// use to_do::to_do_factory;
+// use to_do::structs::traits::create::Create;
+
+// fn main() {
+//   let to_do_item: Result<ItemTypes, &'static str> =
+//     to_do_factory("pending", "wash the dishes");
+
+// // Update this match statement with any new status's from Factory
+//   match to_do_item.unwrap() {
+//     ItemTypes::Pending(item) 
+//       => item.create(&item.super_struct.title),
+//     ItemTypes::Done(item) 
+//       => println!("The {} item is completed!", item.super_struct.title),
+//   }
+// }
 
--- src/state.rs
+++ src/state.rs
@@ -6,11 +6,15 @@ use serde_json::Map;
 use serde_json::value::Value;
 use serde_json::json;
 
-
-// Creates a Map from files so that we can easily access the data
-
+/// The `read_file` function reads a JSON file from disk.
 /// Takes filepath as a string and uses the standard library to open it.
-
+/// Creates a Map from files so that we can easily access the data. 
+///
+/// # Arguments
+/// * file_name (&str): the path to the file being read
+///
+/// # Returns
+/// (Map<String, Value>): returns the contents of the file as a Map
 pub fn read_file(file_name: &str) -> Map<String, Value> {
 
   let mut file = File::open(file_name.to_string()).unwrap(); 
@@ -25,10 +29,27 @@ pub fn read_file(file_name: &str) -> Map<String, Value> {
   file.read_to_string(&mut buffer).unwrap(); 
 
   // converts string from buffer into a JSON value 
-  let json: Value = serde_json::from_str(&buffer).unwrap; 
+  let json: Value = serde_json::from_str(&buffer).unwrap(); 
 
   // JSON value must be an object in order to be returned within a Map
   // This value is then cloned because if not we'd be stuck with a shared reference.
   let state: Map<String, Value> = json.as_object().unwrap().clone(); 
   return state;
+}
+
+/// This function writes a JSON map to file on disk.
+///
+/// # Arguments
+/// * file_name (&str): the path to the file being read
+/// * state (&mut Map<String, Value>): the data being written to disk
+///
+/// # Returns
+/// None
+pub fn write_to_file(file_name: &str, state: &mut Map<String, Value>) {
+
+  // Macro converts Map to JSON value 
+  let new_buffer = json!(state); 
+
+  // turns the JSON value into a string and writes it to the file
+  fs::write(file_name.to_string(), new_buffer.to_string()).expect("Unable to write file"); 
 }
